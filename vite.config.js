e5b7f65e209371a86b33a22372977544176b343b
chemimartinez10 <jmmartineztuarez@gmail.com>
2024-07-09 19:56:40 -0400

    fix to base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base:"/HuskyPages/",
 	publicDir: "public",
 	server: {
 		watch: {
@@ -11,6 +12,5 @@ export default defineConfig({
 		},
 		host: true,
 		port: 3000,
-		base:"/HuskyPages/"
 	},
 })
