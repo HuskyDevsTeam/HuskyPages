eb4d3d14f1808836756d2561cf2a174d7dedd316
chemimartinez10 <jmmartineztuarez@gmail.com>
2025-07-25 18:38:00 -0400

    fix: Adjust team member grid layout to use three columns for better responsiveness

--- src/routes/sections/SectionTeam.svelte
+++ src/routes/sections/SectionTeam.svelte
@@ -72,7 +72,7 @@
 					<h1 class="md:display-small-extrabold headline-medium-bold text-color-primary">{glosary.section_5.article_title}</h1>
 					<p class="text-center body-medium-regular text-color-secondary">{glosary.section_5.article_text}</p>
 				</div>
-				<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 px-8 gap-y-12 gap-x-8">
+				<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 px-8 gap-y-12 gap-x-8">
 					{#each list as member}
 						<div class="flex flex-col gap-4 justify-center items-center text-center">
 							<div class="w-[112px] h-[112px] rounded-full overflow-hidden flex justify-center items-center">
